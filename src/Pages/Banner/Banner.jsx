63f3595bdd2b09861d97df2d4a56bcf2892a88dd
asifrahi0 <asifahemmedrahi0@gmail.com>
2023-06-09 16:12:17 +0600

    banner updated

--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -1,29 +1,26 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from "react-responsive-carousel";
-import "react-awesome-slider/dist/styles.css";
+import AwesomeSlider from 'react-awesome-slider';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+import 'react-awesome-slider/dist/styles.css';
 import img from "../../assets/Banner/volumetric-musical-background-with-treble-clef-notes-generative-ai.jpg";
 import img2 from "../../assets/Banner/volumetric-musical-background-with-treble-clef-notes-generative-ai (1).jpg";
 import img3 from "../../assets/Banner/phone-with-music-icon-headphones-blurred-background-music-listening-concept-copy-space.jpg";
 import img4 from "../../assets/Banner/closeup-dj-working-blue-light.jpg";
 
+const AutoplaySlider = withAutoplay(AwesomeSlider);
+
+
 const Banner = () => {
   return (
-    <div>
-      <Carousel>
-        <div>
-          <img src={img} />
-        </div>
-        <div>
-          <img src={img2} />
-        </div>
-        <div>
-          <img src={img3} />
-        </div>
-        <div>
-          <img src={img4} />
-        </div>
-      </Carousel>
-    </div>
+    <AutoplaySlider
+    play={true}
+    cancelOnInteraction={false} // should stop playing on user interaction
+    interval={1000}
+  >
+    <div data-src={img} />
+    <div data-src={img2} />
+    <div data-src={img3} />
+    <div data-src={img4} />
+  </AutoplaySlider>
   );
 };
 
