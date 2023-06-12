c4abb52786eea48a2e498c8ce7a30d60edc688ab
asifrahi0 <asifahemmedrahi0@gmail.com>
2023-06-12 14:20:39 +0600

    sign up page bug fixed

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -46,7 +46,7 @@ import MyCart from "../Pages/Dashboard/MyCart/MyCart";
     },
     {
       path:"dashboard",
-      element:<Dashboard></Dashboard>,
+      element:<PrivateRoute><Dashboard></Dashboard></PrivateRoute>,
       children: [
         {
           path:"mycart",
