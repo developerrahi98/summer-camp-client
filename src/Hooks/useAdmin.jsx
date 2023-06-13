ddc0cd7daecb68bb0f526791b9f9583a9fc2162c
asifrahi0 <asifahemmedrahi0@gmail.com>
2023-06-14 01:03:21 +0600

    axios interceptors added

---
+++ src/Hooks/useAdmin.jsx
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+import { AuthContext } from "../Providers/AuthProvider";
+
+const useAdmin = () => {
+    const { user } = useContext(AuthContext);
+    const [] = useAxio
+}
+
+export default useAdmin;
